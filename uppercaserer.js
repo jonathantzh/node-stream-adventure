aa27c38acdef2881096a3a2a8f37b6e8fa6422e8
Jon <jtan202@e.ntu.edu.sg>
2015-07-28 22:36:18 +0800

    uppercaserer

--- uppercaserer.js
+++ uppercaserer.js
@@ -2,6 +2,12 @@ var through = require('through2');
 var stream = through(write,end);
 
 function write (buffer, encoding, next) {
-    return buffer.setEncoding(encoding).toString();
-
+    this.push(buffer.toString().toUpperCase());
+    next();
 }
+
+function end (done) {
+    done();
+}
+
+process.stdin.pipe(stream).pipe(process.stdout);
